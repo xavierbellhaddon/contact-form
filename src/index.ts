2c2099d31e341e62ae909d10448742bb7bcdfd5a
Xavier Haddon <xavier.haddon@zapier.com>
2024-09-03 16:54:09 -0400

    refactor

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-const successPopup = document.getElementById("success-popup");
-
 const inputs = document.querySelectorAll("input");
 
 const message = document.getElementById("message");
@@ -8,13 +6,15 @@ const fields: Record<any, any> = [...inputs, message];
 
 const submit = document.getElementById("submit");
 
-const emailRegex = new RegExp(
-  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
-);
-
 const consent = document.getElementById("consent") as HTMLInputElement;
 
 const consentLabel = document.getElementById("consent-label-span");
+
+const successPopup = document.getElementById("success-popup");
+
+const emailRegex = new RegExp(
+  /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+);
 
 consent.addEventListener("change", () => {
   consent.checked
@@ -24,8 +24,29 @@ consent.addEventListener("change", () => {
         'I consent to being contacted by the team <span class="asterisk">*</span></span>');
 });
 
+const handleError = (input, hasError: boolean) => {
+  if (hasError) {
+    document.getElementById(`${input.name}-error`).classList.add("error");
+    document
+      .querySelectorAll(`input[name="${input.name}"]`)
+      .forEach((field) => {
+        field.setAttribute("aria-describedby", `${input.name}-error`);
+        field.setAttribute("aria-invalid", "true");
+      });
+  } else {
+    document.getElementById(`${input.name}-error`).classList.remove("error");
+
+    document
+      .querySelectorAll(`input[name="${input.name}"]`)
+      .forEach((field) => {
+        field.removeAttribute("aria-describedby");
+        field.setAttribute("aria-invalid", "false");
+      });
+  }
+};
+
 const handleSubmit = (e) => {
-  let radioName;
+  let radio;
   let radioIsChecked = false;
   let checkboxIsChecked = true;
   let hasFieldValue = true;
@@ -40,22 +61,16 @@ const handleSubmit = (e) => {
 
     if (field.hasAttribute("required")) {
       if (field.type === "radio") {
-        radioName = field.name;
+        radio = field;
         field.checked && (radioIsChecked = true);
       }
 
       if (field.type === "checkbox") {
         if (!field.checked) {
-          document.getElementById(`${field.name}-error`).classList.add("error");
-          field.setAttribute("aria-describedby", `${field.name}-error`);
-          field.setAttribute("aria-invalid", "true");
+          handleError(field, true);
           checkboxIsChecked = false;
         } else {
-          document
-            .getElementById(`${field.name}-error`)
-            .classList.remove("error");
-          field.removeAttribute("aria-describedby");
-          field.setAttribute("aria-invalid", "false");
+          handleError(field, false);
         }
       }
 
@@ -64,35 +79,19 @@ const handleSubmit = (e) => {
           field.value === "" ||
           (field.name === "email" && !emailRegex.test(field.value))
         ) {
-          document.getElementById(`${field.name}-error`).classList.add("error");
-          field.setAttribute("aria-describedby", `${field.name}-error`);
-          field.setAttribute("aria-invalid", "true");
+          handleError(field, true);
           hasFieldValue = false;
         } else {
-          document
-            .getElementById(`${field.name}-error`)
-            .classList.remove("error");
-          field.removeAttribute("aria-describedby");
-          field.setAttribute("aria-invalid", "false");
+          handleError(field, false);
         }
       }
     }
   });
 
   if (!radioIsChecked) {
-    document.getElementById(`${radioName}-error`).classList.add("error");
-
-    document.querySelectorAll(`input[name="${radioName}"]`).forEach((radio) => {
-      radio.setAttribute("aria-describedby", `${radioName}-error`);
-      radio.setAttribute("aria-invalid", "true");
-    });
+    handleError(radio, true);
   } else {
-    document.getElementById(`${radioName}-error`).classList.remove("error");
-
-    document.querySelectorAll(`input[name="${radioName}"]`).forEach((radio) => {
-      radio.removeAttribute("aria-describedby");
-      radio.setAttribute("aria-invalid", "false");
-    });
+    handleError(radio, false);
   }
 
   if (!radioIsChecked || !checkboxIsChecked || !hasFieldValue) {
